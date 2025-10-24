4d7dcc36740c2c36edff3f610406f31044c9030d
erfanMEH <103627631+erfanMEH@users.noreply.github.com>
2025-10-24 18:25:30 +0330

    Update test2.py

--- test2.py
+++ test2.py
@@ -274,7 +274,8 @@ async def main():
     await app.run_polling()
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     import nest_asyncio
     nest_asyncio.apply()
     asyncio.get_event_loop().run_until_complete(main())
+
